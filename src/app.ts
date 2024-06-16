68921c3169d0e09cc82ab42cb94e5cd967c3630b
wolfiee42 <saifalislam2022@gmail.com>
2024-06-16 16:50:12 +0600

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+
+// middleware
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173'] }))
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my world!')
+})
+// api endpoints
+app.use('/api/v1', router)
+
+
+// error handler
+app.use(globalErrorHandler)
+
+// 404 page
+app.use(notFound)
+
+export default app;
