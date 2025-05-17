e1dd596a7fa2ba46a043ab3eb4b942652dd417d4
wolfiee42 <saifalislam2022@gmail.com>
2025-05-17 11:41:58 +0600

    fix: fixed a bug in route folder

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express'
-import { UserRoute } from '../modules/User/user.route';
 
 const router = Router()
 
 const applicationRoutes = [
-  {
-    path: '/auth',
-    route: UserRoute, // component name
-  },
+  // {
+  //   path: '/auth',
+  //   route: UserRoute, // component name
+  // },
 ]
 
 applicationRoutes.forEach(route => router.use(route.path, route.route))
